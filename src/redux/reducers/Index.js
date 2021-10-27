29be84cea5365d4e6f5aa87d264de4907a49d567
Ashutosh Roy <ashutosh925@gmail.com>
2021-10-27 12:22:29 +0530

    reducer added

--- src/redux/reducers/Index.js
+++ src/redux/reducers/Index.js
@@ -1,9 +1,12 @@
 import Auth from "./Auth";
-
+import nftListReducer from "./nftReducer";
+import projectListReducer from "./projectReducers"
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   auth: Auth,
+  projectReducer: projectListReducer,
+  nftReducer: nftListReducer,
 });
 
 export default rootReducer;
